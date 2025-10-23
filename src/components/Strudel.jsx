0909af406c4b1138c8d48f2f3cf5009979663735
corjy025 <corjy025@mymail.unisa.edu.au>
2025-10-23 22:08:13 +1030

    Creating Components

---
+++ src/components/Strudel.jsx
@@ -0,0 +1,10 @@
+function Strudel() {
+    return (
+        <div className="col-md-8" style={{ maxHeight: "50vh", overflowY: "auto" }}>
+            <div id="editor" />
+            <div id="output" />
+        </div>
+    );
+}
+
+export default Strudel;
