aa6fc27c93ec508800d54aff712d817b87590b03
dyeoi001 <dyeoi001@mymail.unisa.edu.au>
2025-10-24 16:09:27 +1030

    implementing change events to preprocess text area

--- src/components/PreprocessTextArea.jsx
+++ src/components/PreprocessTextArea.jsx
@@ -1,8 +1,8 @@
-function PreprocessTextArea(){
-    return(
+function PreprocessTextArea({ defaultValue, onChange }) {
+    return (
         <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
             <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-            <textarea className="form-control" rows="15" id="proc" ></textarea>
+            <textarea className="form-control" defaultValue={defaultValue} onChange={onChange} rows="15" id="proc" ></textarea>
         </div>
     )
 }
