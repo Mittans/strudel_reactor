95af4a1bede69a7e1528fc3df825e0ea28ad9157
dyeoi001 <dyeoi001@mymail.unisa.edu.au>
2025-10-24 16:09:58 +1030

    implementing start, stop in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { StrudelMirror } from '@strudel/codemirror';
 import { evalScope } from '@strudel/core';
 import { drawPianoroll } from '@strudel/draw';
@@ -19,62 +19,73 @@ const handleD3Data = (event) => {
     console.log(event.detail);
 };
 
-export function SetupButtons() {
+// export function SetupButtons() {
 
-    document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-    document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-    document.getElementById('process').addEventListener('click', () => {
-        Proc()
-    }
-    )
-    document.getElementById('process_play').addEventListener('click', () => {
-        if (globalEditor != null) {
-            Proc()
-            globalEditor.evaluate()
-        }
-    }
-    )
-}
+//     document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
+//     document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
+//     document.getElementById('process').addEventListener('click', () => {
+//         Proc()
+//     }
+//     )
+//     document.getElementById('process_play').addEventListener('click', () => {
+//         if (globalEditor != null) {
+//             Proc()
+//             globalEditor.evaluate()
+//         }
+//     }
+//     )
+// }
 
 
 
-export function ProcAndPlay() {
-    if (globalEditor != null && globalEditor.repl.state.started == true) {
-        console.log(globalEditor)
-        Proc()
-        globalEditor.evaluate();
-    }
-}
+// export function ProcAndPlay() {
+//     if (globalEditor != null && globalEditor.repl.state.started == true) {
+//         console.log(globalEditor)
+//         Proc()
+//         globalEditor.evaluate();
+//     }
+// }
 
-export function Proc() {
+// export function Proc() {
 
-    let proc_text = document.getElementById('proc').value
-    let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
-    ProcessText(proc_text);
-    globalEditor.setCode(proc_text_replaced)
-}
+//     let proc_text = document.getElementById('proc').value
+//     let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
+//     ProcessText(proc_text);
+//     globalEditor.setCode(proc_text_replaced)
+// }
 
-export function ProcessText(match, ...args) {
+// export function ProcessText(match, ...args) {
 
-    let replace = ""
-    // if (document.getElementById('flexRadioDefault2').checked) {
-    //     replace = "_"
-    // }
+//     let replace = ""
+//     // if (document.getElementById('flexRadioDefault2').checked) {
+//     //     replace = "_"
+//     // }
 
-    return replace
-}
+//     return replace
+// }
 
 export default function StrudelDemo() {
 
-const hasRun = useRef(false);
+    const hasRun = useRef(false);
 
-useEffect(() => {
+    const handlePlay = () => {
+        globalEditor.evaluate()
+    }
 
-    if (!hasRun.current) {
-        document.addEventListener("d3Data", handleD3Data);
-        console_monkey_patch();
-        hasRun.current = true;
-        //Code copied from example: https://codeberg.org/uzu/strudel/src/branch/main/examples/codemirror-repl
+    const handleStop = () => {
+        globalEditor.stop()
+    }
+
+    const [songText, setSongText] = useState(stranger_tune)
+
+
+    useEffect(() => {
+
+        if (!hasRun.current) {
+            document.addEventListener("d3Data", handleD3Data);
+            console_monkey_patch();
+            hasRun.current = true;
+            //Code copied from example: https://codeberg.org/uzu/strudel/src/branch/main/examples/codemirror-repl
             //init canvas
             const canvas = document.getElementById('roll');
             canvas.width = canvas.width * 2;
@@ -100,45 +111,44 @@ useEffect(() => {
                     await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
                 },
             });
-            
-        document.getElementById('proc').value = stranger_tune
-        SetupButtons()
-        Proc()
-    }
 
-}, []);
+            document.getElementById('proc').value = stranger_tune
+
+        }
+        globalEditor.setCode(songText);
+    }, [songText]);
 
 
-return (
-    <div>
-        <h2>Strudel Demo</h2>
-        <main>
+    return (
+        <div>
+            <h2>Strudel Demo</h2>
+            <main>
 
-            <div className="container-fluid">
-                <div className="row">
-                        <PreprocessTextArea/>
-                    <div className="col-md-4">
-                        <nav>
-                            <ProcessButtons/>
-                            <br />
-                            <PlayButtons/>
-                        </nav>
+                <div className="container-fluid">
+                    <div className="row">
+                        <PreprocessTextArea defaultValue={songText} onChange={(e) => setSongText(e.target.value)} />
+                        <div className="col-md-4">
+                            <nav>
+                                <ProcessButtons />
+                                <br />
+                                <PlayButtons onPlay={handlePlay} onStop={handleStop} />
+                            </nav>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                            <div id="editor" />
+                            <div id="output" />
+                        </div>
+                        <div className="col-md-4">
+                            <AUXControls />
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <div id="editor" />
-                        <div id="output" />
-                    </div>
-                    <div className="col-md-4">
-                        <AUXControls/>
-                    </div>
-                </div>
-            </div>
-            <canvas id="roll"></canvas>
-        </main >
-    </div >
-);
+                <canvas id="roll"></canvas>
+            </main >
+        </div >
+    );
 
 
 }
