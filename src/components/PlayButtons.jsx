6be6ab2d2c668ad087214473dcb688909a47f874
dyeoi001 <dyeoi001@mymail.unisa.edu.au>
2025-10-24 14:09:10 +1030

    implementing component for playing and pausing

---
+++ src/components/PlayButtons.jsx
@@ -0,0 +1,9 @@
+function PlayButtons(){
+    return(
+        <div>                            
+            <button id="play" className="btn btn-outline-primary">Play</button>
+            <button id="stop" className="btn btn-outline-primary">Stop</button>
+        </div>
+    );
+}
+export default PlayButtons
