2ddfd2c974876dc25d0da6c75af9313853766686
dyeoi001 <dyeoi001@mymail.unisa.edu.au>
2025-10-24 16:10:15 +1030

    adding stop and start functionality

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -1,8 +1,8 @@
-function PlayButtons(){
-    return(
-        <div>                            
-            <button id="play" className="btn btn-outline-primary">Play</button>
-            <button id="stop" className="btn btn-outline-primary">Stop</button>
+function PlayButtons({ onPlay, onStop }) {
+    return (
+        <div>
+            <button id="play" className="btn btn-outline-primary" onClick={onPlay}>Play</button>
+            <button id="stop" className="btn btn-outline-primary" onClick={onStop}>Stop</button>
         </div>
     );
 }
