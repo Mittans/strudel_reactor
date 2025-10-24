7cb06942b3254ff13479f67eb507e37be1fcbf77
dyeoi001 <dyeoi001@mymail.unisa.edu.au>
2025-10-24 14:08:52 +1030

    Implementing aux controls component for sound and mute

---
+++ src/components/AUX_Controls.jsx
@@ -0,0 +1,20 @@
+function AUX_Controls(){
+    return(
+        <>
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={"#"} defaultChecked />
+                <label className="form-check-label" htmlFor="flexRadioDefault1">
+                    p1: ON
+                </label>
+            </div>
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={"#"} />
+                <label className="form-check-label" htmlFor="flexRadioDefault2">
+                    p1: HUSH
+                </label>
+            </div>
+        </>
+    );
+}
+
+export default AUX_Controls
