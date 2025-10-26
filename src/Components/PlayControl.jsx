6e4e5a810e5d943c50e300fde451ed525e410b4a
Nisha <kafny002@mymail.unisa.edu.au>
2025-10-26 20:47:28 +1030

    Added PlayControl component and import in App.js
    Created a PlayControl component for radio button controls and imported it into App.js.

---
+++ src/Components/PlayControl.jsx
@@ -0,0 +1,21 @@
+
+function PlayControl() {
+    return (
+        <> {/* <> called React fragment, empty tags lets group multiple elements without having to use div */}
+
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
+                <label className="form-check-label" htmlFor="flexRadioDefault1">
+                    p1: ON
+                </label>
+            </div>
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
+                <label className="form-check-label" htmlFor="flexRadioDefault2">
+                    p1: HUSH
+                </label>
+            </div>
+        </>
+    );
+}
+export default PlayControl;
