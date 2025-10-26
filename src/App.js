688ba8ca63373cfcf5a258cd79ef3e49c252726f
rebbr001 <rebbr001@mymail.unisa.edu.au>
2025-10-26 21:19:38 +1030

    Made a 2x2 grid layout, with neon border

--- src/App.js
+++ src/App.js
@@ -37,30 +37,59 @@ export default function StrudelDemo() {
         strudelRef.current?.evaluate();
     }
 
+// return (
+//     <div>
+//         <PageTitle/>
+//         <main>
+//             <div className="container-fluid">
+//                 <div className="row">
+//                     <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+//                         <TextPreprocessor defaultText={stranger_tune} onchange={e => setStrudelCode(e.target.value)}/>
+//                     </div>
+//                     <div className="col-md-4">
+//                         <AudioControls
+//                         handlePlay={handlePlay}
+//                         handleStop={handleStop}
+//                         handlePreprocess={handlePreprocess}
+//                         handleProcPlay={handleProcPlay}
+//                         />
+//                     </div>
+//                 </div>
+//                 <div className="row">
+//                     <EditorArea onProc={handleProc}/>
+//                 </div>
+//             </div>
+//             <StrudelPlayer strudelCode={stranger_tune} strudelRef={strudelRef}/>
+//         </main >
+//     </div >
+// );
+
 return (
-    <div>
-        <PageTitle/>
-        <main>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <TextPreprocessor defaultText={stranger_tune} onchange={e => setStrudelCode(e.target.value)}/>
-                    </div>
-                    <div className="col-md-4">
-                        <AudioControls
-                        handlePlay={handlePlay}
-                        handleStop={handleStop}
-                        handlePreprocess={handlePreprocess}
-                        handleProcPlay={handleProcPlay}
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <EditorArea onProc={handleProc}/>
-                </div>
-            </div>
-            <StrudelPlayer strudelCode={stranger_tune} strudelRef={strudelRef}/>
-        </main >
-    </div >
+  <div className="container py-1">
+    <PageTitle />
+    <br/>
+    <div className="row g-4">
+      <div className="col-6">
+        <div className="square-box d-flex align-items-center justify-content-center">
+          <p>Square 1</p>
+        </div>
+      </div>
+      <div className="col-6">
+        <div className="square-box d-flex align-items-center justify-content-center">
+          <p>Square 2</p>
+        </div>
+      </div>
+      <div className="col-6">
+        <div className="square-box d-flex align-items-center justify-content-center">
+          <p>Square 3</p>
+        </div>
+      </div>
+      <div className="col-6">
+        <div className="square-box d-flex align-items-center justify-content-center">
+          <p>Square 4</p>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 }
