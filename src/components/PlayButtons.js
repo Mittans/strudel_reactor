c0d1603013a7cf6d99554a97a38b8e36a616d1de
Yujia Gu <yujiagu1010@gmail.com>
2025-10-28 10:04:36 +1030

    refactor: convert button components to use props callbacks

--- src/components/PlayButtons.js
+++ src/components/PlayButtons.js
@@ -2,16 +2,12 @@ import { FaRegCirclePlay } from "react-icons/fa6";
 import { FaRegCircleStop } from "react-icons/fa6";
 import { IoMusicalNotes } from "react-icons/io5";
 
-function PlayButtons(){
+function PlayButtons({ onPlay, onStop }){
   return (
-      <div className="card bg-dark text-light border-0 rounded-4 p-4">
+      <div className="card bg-dark text-light border-0 rounded-4 p-3 mt-3">
         <div className="d-flex justify-content-between align-items-center">
-          <button className="icon-btn" id="stop">
-            <FaRegCircleStop className="control-icon" />
-          </button>
-          <button className="icon-btn" id="play">
-            <FaRegCirclePlay className="control-icon" />
-          </button>
+          <FaRegCircleStop className="control-icon" onClick={onStop}/>
+          <FaRegCirclePlay className="control-icon" onClick={onPlay}/>
           <span className="status-pill"><IoMusicalNotes /> Live</span>
         </div>
       </div>
--- src/components/ProcButtons.js
+++ src/components/ProcButtons.js
@@ -1,16 +1,16 @@
 import { RiMagicLine } from "react-icons/ri";
 import { FaPlayCircle } from "react-icons/fa";
 
-function ProcButtons() {
+function ProcButtons({ onPreprocess, onProcAndPlay }) {
   return (
-    <div className="card bg-dark text-light border-0 rounded-4 p-4">
+    <div className="card bg-dark text-light border-0 rounded-4 p-3">
       <div className="d-flex justify-content-between align-items-center">
-        <button id="process" className="proc-btn">
+        <button className="proc-btn" onClick={onPreprocess}>
           <RiMagicLine className="proc-icon" />
           <span className="ms-2">Preprocess</span>
         </button>
 
-        <button id="process_play" className="proc-btn">
+        <button className="proc-btn" onClick={onProcAndPlay}>
           <FaPlayCircle className="proc-icon" />
           <span className="ms-2">Proc & Play</span>
         </button>
