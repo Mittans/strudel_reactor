4fa93badd22609d68639c3bd77abd401a645ea25
Yujia Gu <yujiagu1010@gmail.com>
2025-10-27 11:07:01 +1030

    Update PlayButtons layout and styles

--- src/components/PlayButtons.js
+++ src/components/PlayButtons.js
@@ -1,11 +1,16 @@
+import { FaRegCirclePlay } from "react-icons/fa6";
+import { FaRegCircleStop } from "react-icons/fa6";
+import { IoMusicalNotes } from "react-icons/io5";
+
 function PlayButtons(){
   return (
-    <>
-      <div className="btn-group" role="group">
-        <button id="play" className="btn btn-outline-primary">Play</button>
-        <button id="stop" className="btn btn-outline-primary">Stop</button>
+      <div className="card bg-dark text-light border-0 rounded-4 p-4">
+        <div className="d-flex justify-content-between align-items-center">
+          <FaRegCircleStop className="control-icon" />
+          <FaRegCirclePlay className="control-icon" />
+          <span className="status-pill"><IoMusicalNotes /> Live</span>
+        </div>
       </div>
-    </>
   )
 }
 export default PlayButtons;
