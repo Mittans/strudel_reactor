cfcc8f51e8ca60c8f273d877daa1e6dec67c185a
Yujia Gu <yujiagu1010@gmail.com>
2025-10-27 14:40:38 +1030

    Update ProcButtons layout and styles

--- src/components/ProcButtons.js
+++ src/components/ProcButtons.js
@@ -1,11 +1,21 @@
-function ProcButtons(){
+import { RiMagicLine } from "react-icons/ri";
+import { FaPlayCircle } from "react-icons/fa";
+
+function ProcButtons() {
   return (
-    <>
-      <div className="btn-group" role="group">
-        <button id="process" className="btn btn-outline-primary">Preprocess</button>
-        <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+    <div className="card bg-dark text-light border-0 rounded-4 p-4">
+      <div className="d-flex justify-content-between align-items-center">
+        <button id="process" className="proc-btn">
+          <RiMagicLine className="proc-icon" />
+          <span className="ms-2">Preprocess</span>
+        </button>
+
+        <button id="process_play" className="proc-btn">
+          <FaPlayCircle className="proc-icon" />
+          <span className="ms-2">Proc & Play</span>
+        </button>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 export default ProcButtons;
