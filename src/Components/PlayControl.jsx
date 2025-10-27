f97097a5c0b40b778e8cf1cf0cb5f725cfbbf197
Nisha <kafny002@mymail.unisa.edu.au>
2025-10-27 18:57:44 +1030

    Update input types with min/max/step

--- src/Components/PlayControl.jsx
+++ src/Components/PlayControl.jsx
@@ -7,37 +7,30 @@ function PlayControl() {
 
             <div className="input-group mb-3">
                 <span className="input-group-text" id="cpm_label">SetCPM</span>
-                <input type="text" class="form-control" id="cpm_text_input" placeholder="120" aria-label="120" aria-describedby="cpm_label" />
-
+                <input type="number" class="form-control" id="cpm_text_input" placeholder="120" aria-label="120" aria-describedby="cpm_label" min="0" max="500" step="10" />
             </div>
-
-            <label for="volume" class="form-label">Volume Slider</label>
-            <input type="range" className="form-range" min="0" max="1" step="0.05" id="volume_range"></input>
 
 
             <div className="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="s1" />
-                <label class="form-check-label" for="s1">
+                <input className="form-check-input" type="checkbox" value="" id="s1" />
+                <label className="form-check-label" for="s1">
                     s1
                 </label>
             </div>
 
-
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="d1" />
-                <label class="form-check-label" for="d1">
+                <input className="form-check-input" type="checkbox" value="" id="d1" />
+                <label className="form-check-label" for="d1">
                     d1
                 </label>
             </div>
 
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="d2" />
-                <label class="form-check-label" for="d2">
+                <input className="form-check-input" type="checkbox" value="" id="d2" />
+                <label className="form-check-label" for="d2">
                     d2
                 </label>
             </div>
-
-
 
 
 
