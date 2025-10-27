f37d56078ecc66cdceea4ca5b35a2f26ab8da900
Nisha <kafny002@mymail.unisa.edu.au>
2025-10-27 18:01:21 +1030

    Update PlayControl to use input group for CPM
    Replaced radio button controls with an input group for setting CPM in PlayControl.jsx.

--- src/Components/PlayControl.jsx
+++ src/Components/PlayControl.jsx
@@ -3,18 +3,13 @@ function PlayControl() {
     return (
         <> {/* <> called React fragment, empty tags lets group multiple elements without having to use div */}
 
-            <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                    p1: ON
-                </label>
-            </div>
-            <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                <label className="form-check-label" htmlFor="flexRadioDefault2">
-                    p1: HUSH
-                </label>
-            </div>
+
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="cpm_label">SetCPM</span>
+                <input type="text" class="form-control" placeholder="120" aria-label="120" aria-describedby="cpm_label" />
+            </div >
+
+
         </>
     );
 }
