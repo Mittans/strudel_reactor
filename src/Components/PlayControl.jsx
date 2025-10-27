9a9f1e2431a49b8b98f75d9ea11c51ce0e0ac347
Nisha <kafny002@mymail.unisa.edu.au>
2025-10-27 18:40:07 +1030

    Add volume slider and checkboxes to PlayControl
    Added a volume slider and several checkbox controls (s1, d1, d2) to the PlayControl component.

--- src/Components/PlayControl.jsx
+++ src/Components/PlayControl.jsx
@@ -4,13 +4,45 @@ function PlayControl() {
         <> {/* <> called React fragment, empty tags lets group multiple elements without having to use div */}
 
 
+
             <div className="input-group mb-3">
                 <span className="input-group-text" id="cpm_label">SetCPM</span>
-                <input type="text" class="form-control" placeholder="120" aria-label="120" aria-describedby="cpm_label" />
-            </div >
+                <input type="text" class="form-control" id="cpm_text_input" placeholder="120" aria-label="120" aria-describedby="cpm_label" />
+
+            </div>
+
+            <label for="volume" class="form-label">Volume Slider</label>
+            <input type="range" className="form-range" min="0" max="1" step="0.05" id="volume_range"></input>
+
+
+            <div className="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="s1" />
+                <label class="form-check-label" for="s1">
+                    s1
+                </label>
+            </div>
+
+
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="d1" />
+                <label class="form-check-label" for="d1">
+                    d1
+                </label>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="d2" />
+                <label class="form-check-label" for="d2">
+                    d2
+                </label>
+            </div>
+
+
+
 
 
         </>
     );
 }
 export default PlayControl;
+
