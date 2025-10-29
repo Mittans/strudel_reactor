b18961f7d76eba7449d4c2cb1cd777a308297f68
Nguyen Tuan Hung (Stefan) Phan <phany036@mymail.unisa.edu.au>
2025-10-29 13:49:34 +1030

    Refactor the code in useStrudel by created a helper function to apply updated code to the song

--- src/hooks/useStrudel.js
+++ src/hooks/useStrudel.js
@@ -8,14 +8,14 @@ export function useStrudel(intitalTune) {
 
   const [bpm, setBpm] = useState(140);
   const [volume, setVol] = useState(0.8);
-  const [pattern, setPattern] = useState("0");
-  const [bass, setBass] = useState("0");
+  const [pattern, setPattern] = useState(0);
+  const [bass, setBass] = useState(0);
 
   const hasInit = useRef(false);
 
   const [procValue, setProcValue] = useState(intitalTune);
 
-  // Intialize Strudel
+  // ---- Intialize Strudel ------------------------------------------------------
   useEffect(() => {
     if (!hasInit.current) {
       hasInit.current = true;
@@ -25,15 +25,10 @@ export function useStrudel(intitalTune) {
     }
   }, [intitalTune]);
 
-  function changeTempo(newBpm) {
-    setBpm(newBpm);
-
+  // ---- Helper Functions ------------------------------------------------------
+  function applyUpdatedCode(currentText, replacedText) {
     setProcValue((prevCode) => {
-      // replace the current tempo with the new one
-      let updatedCode = prevCode.replace(
-        /setcps\([^)]*\)/g, // look for setcps\n + [^)] match everything except + "*" repeat that any number of times + \) matching the closing parentheses + /g means find all matches)
-        `setcps(${newBpm}/60/4)`
-      );
+      let updatedCode = prevCode.replace(currentText, replacedText);
 
       // update the code
       strudelActions.setCode(updatedCode);
@@ -46,6 +41,13 @@ export function useStrudel(intitalTune) {
       // update the textarea
       return updatedCode;
     });
+  }
+
+  // ---- Updaters: allow user to update different setting to the song ------------------------------------------------------
+  function changeTempo(newBpm) {
+    setBpm(newBpm);
+
+    return applyUpdatedCode(/setcps\([^)]*\)/g, `setcps(${newBpm}/60/4)`);
   }
 
   function changeGainPattern(newGainPattern) {
@@ -54,24 +56,10 @@ export function useStrudel(intitalTune) {
     // Convert to integer
     const patternIndex = parseInt(newGainPattern);
 
-    // TODO: Explain the replace method and refactor the changeTempo and changePattern
-    setProcValue((prevCode) => {
-      let updatedCode = prevCode.replace(
-        /const\s+pattern\s*=\s*\d+/,
-        `const pattern = ${patternIndex}`
-      );
-
-      // update the code
-      strudelActions.setCode(updatedCode);
-
-      // re-play the song if the song is currently playing
-      if (isPlaying) {
-        strudelActions.evaluate();
-      }
-
-      // update the textarea
-      return updatedCode;
-    });
+    return applyUpdatedCode(
+      /const\s+pattern\s*=\s*\d+/,
+      `const pattern = ${patternIndex}`
+    );
   }
 
   function changeBass(newBass) {
@@ -79,25 +67,10 @@ export function useStrudel(intitalTune) {
 
     // Convert to integer
     const bassIndex = parseInt(newBass);
-
-    // TODO: Explain the replace method and refactor the changeTempo and changePattern
-    setProcValue((prevCode) => {
-      let updatedCode = prevCode.replace(
-        /const\s+bass\s*=\s*\d+/,
-        `const bass = ${bassIndex}`
-      );
-
-      // update the code
-      strudelActions.setCode(updatedCode);
-
-      // re-play the song if the song is currently playing
-      if (isPlaying) {
-        strudelActions.evaluate();
-      }
-
-      // update the textarea
-      return updatedCode;
-    });
+    return applyUpdatedCode(
+      /const\s+bass\s*=\s*\d+/,
+      `const bass = ${bassIndex}`
+    );
   }
 
   function changeVolume(v) {
