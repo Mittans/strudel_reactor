ef094e8688208ba7d23535a5090f6a80fbc7afa0
patvn002 <patvn002@mymail.unisa.edu.au>
2025-10-31 16:06:49 +1030

    cleaned up app by moving buttons and text areas into separate components
    moved play/pause stuff into DJControls, split play and pause buttons into their own PauseAndPlay component, moved preprocess + play buttons into ProcButtons moved controls text area into ControlsTextArea in the components folder

---
+++ src/components/ProcButtons.js
@@ -0,0 +1,15 @@
+function PauseAndPlay() {
+    return (
+        <>
+            <nav className="btn-group" role="group" aria-label="Transport controls">
+                <button id="process" className="btn btn-outline-primary">Preprocess</button>
+                <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+            </nav>
+        </>
+    );
+}
+export default PauseAndPlay;
+
+
+
+
