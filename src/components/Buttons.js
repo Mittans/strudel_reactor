c9f241bcd19a4cb526c9cf8bc6ddcf3e951695bd
SainaDrall <drasy014@mymail.unisa.edu.au>
2025-10-31 19:51:22 +1030

    Created new components folder and added Buttons.js, ProcButtons.js, and DJControls.js

---
+++ src/components/Buttons.js
@@ -0,0 +1,12 @@
+function Buttons() {
+	return (
+		<>
+			<div className="btn-group" role="group" aria-label="Basic mixed styles example">
+				<button id="play" className="btn btn-outline-primary">Play</button>
+				<button id="stop" className="btn btn-outline-primary">Stop</button>
+			</div>	
+		</>
+	);
+}
+
+export default Buttons;
