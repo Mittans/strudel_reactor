82553b4419518ab2cfe2a043fe3a193154f8e642
SainaDrall <drasy014@mymail.unisa.edu.au>
2025-11-02 21:25:17 +1030

    Added sample and drum checkboxes (s1, d1, d2) to DJControls component.

--- src/components/DJControls.js
+++ src/components/DJControls.js
@@ -22,6 +22,28 @@ function DJControls() {
                 step="0.01"
                 id="volume_range"
             />
+
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="s1" />
+                <label className="form-check-label" htmlFor="s1">
+                    s1
+                </label>
+            </div>
+
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="d1" />
+                <label className="form-check-label" htmlFor="d1">
+                    d1
+                </label>
+            </div>
+
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="d2" />
+                <label className="form-check-label" htmlFor="d2">
+                    d2
+                </label>
+            </div>
+
         </>
     );
 }
