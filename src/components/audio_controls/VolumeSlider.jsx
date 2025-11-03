b322655338f602ee7a84a334ddc7c90ee3da52b9
Sam-colss002 <163492479+Sam-colss002@users.noreply.github.com>
2025-11-03 15:19:03 +1030

    coloured scrollbars to match UI
    also added extra colour and detail to sliders

--- src/components/audio_controls/VolumeSlider.jsx
+++ src/components/audio_controls/VolumeSlider.jsx
@@ -4,7 +4,7 @@ function VolumeSlider({ volume, setVolume, onHandleVolume}) {
         <div className="input-group mb-4">
             <span className="input-group-text menu_label_subject" id="volume_label">Volume</span>
             {/* if nothing additional is added, e is passed directly (think self and python functions) */}
-            <input type="range" className="form-control menu_label_value" min="0" max="1" value={volume} step="0.01" defaultValue="0.5" id="volume_range" onChange={(e) => {
+            <input type="range" className="col-lg menu_label_value slider" min="0" max="1" value={volume} step="0.01" defaultValue="0.5" id="volume_range" onChange={(e) => {
                 setVolume(e.target.value);
                 onHandleVolume(e);
             }}/>
