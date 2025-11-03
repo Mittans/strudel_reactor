ca23636016878e4a51d9f8399f77e04037fa7a61
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-03 15:01:49 +1030

    Changed styling again

--- src/components/play-buttons.jsx
+++ src/components/play-buttons.jsx
@@ -3,11 +3,11 @@ import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 function PlayButtons({ onPlay, onPause }) {
     return (
         <>
-            <div className="btn-group btn-group-lg pt-3">
-                <button id="play" className="btn btn-outline-dark bg-secondary" title="Play" onClick={onPlay}><BsFillPlayFill /></button>
-                <button id="stop" className="btn btn-outline-dark bg-secondary" title="Pause" onClick={onPause}><BsFillPauseFill /></button>
-                <button id="process" className="btn btn-outline-dark bg-secondary">Preprocess</button>
-                <button id="process_play" className="btn btn-outline-dark bg-secondary">Proc & Play</button>
+            <div className="btn-group btn-group-lg gap-1 pt-3">
+                <button id="play" className="btn btn-secondary" title="Play" onClick={onPlay}><BsFillPlayFill /></button>
+                <button id="stop" className="btn btn-secondary" title="Pause" onClick={onPause}><BsFillPauseFill /></button>
+                <button id="process" className="btn btn-secondary">Preprocess</button>
+                <button id="process_play" className="btn btn-secondary">Proc & Play</button>
             </div>
         </>
     );
