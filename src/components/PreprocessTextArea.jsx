7c4dd356cd631de4c2de2cc67fd21d3ee37280ae
dyeoi001 <dyeoi001@mymail.unisa.edu.au>
2025-11-03 16:52:00 +1030

    fixing layout

--- src/components/PreprocessTextArea.jsx
+++ src/components/PreprocessTextArea.jsx
@@ -1,6 +1,6 @@
 function PreprocessTextArea({ defaultValue, onChange }) {
     return (
-        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+        <div style={{ maxHeight: '50vh', overflowY: 'auto' }}>
             <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
             <textarea className="form-control" defaultValue={defaultValue} onChange={onChange} rows="15" id="proc" ></textarea>
         </div>
