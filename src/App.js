280c66bb2041b6daa4289be22d41897a6b106b84
dadja001 <dadja001@mymail.unisa.edu.au>
2025-11-03 20:21:35 +1030

    Split out App.js into components. Note: ON/HUSH not currently working.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import { registerSoundfonts } from '@strudel/soundfonts';
 import { stranger_tune } from './tunes';
 import console_monkey_patch, { getD3Data } from './console-monkey-patch';
 
+import PreprocessTextArea from './components/PreprocessTextArea';
+import ProcessButtons from './components/ProcessButtons';
+import PlayButtonsGroup from './components/PlayButtonsGroup';
+import ControlsSection from './components/ControlsSection';
+
 let globalEditor = null;
 
 const handleD3Data = (event) => {
@@ -114,17 +119,13 @@ return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-                        <textarea className="form-control" rows="15" id="proc" ></textarea>
+                        <PreprocessTextArea />
                     </div>
                     <div className="col-md-4">
-
                         <nav>
-                            <button id="process" className="btn btn-outline-primary">Preprocess</button>
-                            <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+                            <ProcessButtons />
                             <br />
-                            <button id="play" className="btn btn-outline-primary">Play</button>
-                            <button id="stop" className="btn btn-outline-primary">Stop</button>
+                            <PlayButtonsGroup />
                         </nav>
                     </div>
                 </div>
@@ -134,18 +135,7 @@ return (
                         <div id="output" />
                     </div>
                     <div className="col-md-4">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
-                            <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                p1: ON
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
-                            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                p1: HUSH
-                            </label>
-                        </div>
+                        <ControlsSection />
                     </div>
                 </div>
             </div>
---
+++ src/components/PlayButtonsGroup.jsx
@@ -0,0 +1,12 @@
+function PlayButtonsGroup() {
+  return (
+      <>
+        <div className="btn-group" role="group" aria-label="Play buttons group">
+            <button id="play" className="btn btn-outline-primary">Play</button>
+            <button id="stop" className="btn btn-outline-primary">Stop</button>
+        </div>
+      </>
+  );
+}
+
+export default PlayButtonsGroup;
