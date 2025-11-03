a908eacce6e1fd01dfeca418c9c3fc24976ea74b
dadja001 <dadja001@mymail.unisa.edu.au>
2025-11-03 21:05:52 +1030

    Add extra controls (front end only)

--- src/components/ControlsSection.jsx
+++ src/components/ControlsSection.jsx
@@ -1,16 +1,36 @@
 function ControlsSection() {
   return (
       <>
+          <div className="input-group mb-3">
+              <span className="input-group-text" id="cpm_label">Set CPM</span>
+              <input type="text" className="form-control" id="cpm_text_input" placeholder="CPM (cycles per minute) e.g. 120" aria-label="cpm" aria-describedby="cpm_label" />
+          </div>
+
+          <label for="volume_range" className="form-label">Volume</label>
+          <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" />
+
           <div className="form-check">
-              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-              <label className="form-check-label" htmlFor="flexRadioDefault1">
-                  p1: ON
+              <input className="form-check-input" type="checkbox" value="" id="baseline" />
+              <label className="form-check-label" for="baseline">
+                  baseline
               </label>
           </div>
           <div className="form-check">
-              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-              <label className="form-check-label" htmlFor="flexRadioDefault2">
-                  p1: HUSH
+              <input className="form-check-input" type="checkbox" value="" id="main_arp" />
+              <label className="form-check-label" for="main_arp">
+                  main_arp
+              </label>
+          </div>
+          <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="drums" />
+              <label className="form-check-label" for="drums">
+                  drums
+              </label>
+          </div>
+          <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="drums2" />
+              <label className="form-check-label" for="drums2">
+                  drums2
               </label>
           </div>
       </>
