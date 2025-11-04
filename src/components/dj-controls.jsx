7563c17c9b50bc311ed8074312ad4a478e7da5da
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-04 18:18:16 +1030

    Changed colours of switch

--- src/components/dj-controls.jsx
+++ src/components/dj-controls.jsx
@@ -1,11 +1,12 @@
 import InstrumentControls from "./instrument-controls";
+import './component-styling.css'
 
 function DJControls() {
     return (
         <>
             <div className="form-check form-switch">
-                <label class="form-check-label" for="instrumentSwitch">p1</label>
-                <input class="form-check-input" type="checkbox" role="switch" id="instrumentSwitch" />
+                <label class="form-check-label px-2" for="instrumentSwitch">p1</label>
+                <input class="form-check-input form-switch bg-secondary border-dark" type="checkbox" role="switch" id="instrumentSwitch" />
             </div>
 
             <InstrumentControls/>
