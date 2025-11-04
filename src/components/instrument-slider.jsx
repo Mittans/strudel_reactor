ab10d143e486aa4923a0331d4b6742cdabe672f4
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-04 18:31:42 +1030

    Implemented custom styling

--- src/components/instrument-slider.jsx
+++ src/components/instrument-slider.jsx
@@ -1,8 +1,10 @@
+import './component-styling.css'
+
 function InstrumentSlider( {instrument, text} ) {
     return (
         <>
             <label for="instrumentRange" class="form-label pt-3">{instrument} {text}</label>
-            <input type="range" class="form-range" min="0" max="100" step="0.5" id="instrumentRange"></input>
+            <input type="range" class="form-range slider" min="0" max="100" step="0.5" id="instrumentRange"></input>
         </>
     )
 }
