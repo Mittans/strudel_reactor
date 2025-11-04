927cdc2b14a7437f18308f5bd966ef4beb37dfa4
burcj013 <burcj013@mymail.unisa.edu.au>
2025-11-04 18:42:42 +1030

    Changed input's number of lines to fit better on the page

--- src/components/PreprocessText.jsx
+++ src/components/PreprocessText.jsx
@@ -1,7 +1,9 @@
+
 function PreprocessText({ defaultValue, onChange }) {
+
     return (
         <div>   
-            <textarea style={{ fontFamily: 'Lucida Console, monospace' }} className="form-control bg-dark text-light border-secondary rounded-top-0" rows="15" defaultValue={defaultValue} onChange={onChange} id="proc" ></textarea>
+            <textarea style={{ fontFamily: 'Lucida Console, monospace' }} className="form-control bg-dark text-light border-secondary rounded-top-0" rows="13" defaultValue={defaultValue} onChange={onChange} id="proc" ></textarea>
         </div>
     );
 }
