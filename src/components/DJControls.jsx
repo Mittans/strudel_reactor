6cb63a554e10fa96c13cee455a0a2bfb640bebeb
Ruohan Wang <rayyy020611@gmail.com>
2025-11-05 16:25:03 +1030

    rewirte playbuttons in react way

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -5,25 +5,25 @@ function DJControls() {
               <span className="input-group-text" id="basic-addon1">set CPM</span>
               <input type="text" className="form-control" id="cpm_text_input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label" />
 
-              <label for="volume_range" className="form-label">Volume</label>
+              <label htmlFor="volume_range" className="form-label">Volume</label>
               <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" />
 
 
               <div className="form-check">
                   <input className="form-check-input" type="checkbox" value="" id="s1" />
-                  <label className="form-check-label" for="s1">
+                  <label className="form-check-label" htmlFor="s1">
                           s1
                       </label>
               </div>
               <div className="form-check">
                   <input className="form-check-input" type="checkbox" value="" id="d1" />
-                  <label className="form-check-label" for="d1">
+                  <label className="form-check-label" htmlFor="d1">
                          d1
                       </label>
               </div>
               <div className="form-check">
                   <input className="form-check-input" type="checkbox" value="" id="d2" />
-                  <label className="form-check-label" for="d2">
+                  <label className="form-check-label" htmlFor="d2">
                       d2
                   </label>
               </div>
