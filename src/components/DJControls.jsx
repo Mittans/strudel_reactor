4dfa5dbefcb1154ed53ba6a8e7f3425f16cabf1e
Den5122 <beldy019@mymail.unisa.edu.au>
2025-11-07 03:07:03 +1030

    deleted java stuff and add functionatily to buttons

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -8,24 +8,24 @@ function DJControls() {
         
     </div>
     
-        <label for="volume_range" className="form-label">Volume</label>
+        <label htmlFor="volume_range" className="form-label">Volume</label>
         <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"></input>
 
         <div className="form-check">
   <input className="form-check-input" type="checkbox" value="" id="checkDefault"/>
-  <label className="form-check-label" for="s1">
+  <label className="form-check-label" htmlFor="s1">
     s1
   </label>
 </div>
 <div className="form-check">
   <input className="form-check-input" type="checkbox" value="" id="checkDefault" />
-  <label className="form-check-label" for="d1">
+  <label className="form-check-label" htmlFor="d1">
     d1
   </label>
 </div>
 <div className="form-check">
   <input className="form-check-input" type="checkbox" value="" id="checkDefault" />
-  <label className="form-check-label" for="d2">
+  <label className="form-check-label" htmlFor="d2">
     d2
   </label>
 </div>
