3c3206b35689fc3902c481fe17cf9825c48c313e
KhupSawm <trnlsun@gmail.com>
2025-11-08 13:52:13 +1030

    Added styling to buttons, implemented circular button and hover effects. Added title hover text effects.

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -3,11 +3,11 @@
 export default function Controls({ onProcess, onProcPlay, onPlay, onStop, replay }) {
     return (
         <div className="music-controls d-flex justify-content-center align-items-center gap-3 my-3">
-            <button className="control-btn" onClick={onProcess}>Process</button>
-            <button className="control-btn" onClick={onProcPlay}>Proc&Play</button>
-            <button className="control-btn" onClick={onStop}><FaStop /></button>
-            <button className="control-btn play-btn" onClick={onPlay}><FaPlay /></button>
-            <button className="control-btn" onClick={replay}><FaRedo /></button>    
+            <button className="control-btn" title="Process" onClick={onProcess}>Process</button>
+            <button className="control-btn" title="Proc&Play" onClick={onProcPlay}>Proc&Play</button>
+            <button className="control-btn stop-btn" title="Stop" onClick={onStop}><FaStop /></button>
+            <button className="control-btn play-btn" title="Play" onClick={onPlay}><FaPlay /></button>
+            <button className="control-btn redo-btn" title="Reload" onClick={replay}><FaRedo /></button>    
         </div>
     );
 }
